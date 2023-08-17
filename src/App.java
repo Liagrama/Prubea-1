bba554cc60bf3a9bff1ede8459ac4afaed6849b5
liagrama <a01641017@tec.mx>
2023-08-16 18:55:20 -0600

    Mis canciones favoritas

--- src/App.java
+++ src/App.java
@@ -1,7 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Jumpa"); 
-
+        //high school musical
+        System.out.println("Gotta go my own way"); 
+        //hamilton
+        System.out.println("satisfied"); 
+//mamma mia
+System.out.println("why did it have to be me"); 
+//harry styles
+System.out.println("satellite"); 
+//declan mckenna
+System.out.println("Brazil"); 
     
     }
 }
